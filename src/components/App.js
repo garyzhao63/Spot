07f4d81748b3979950df86057a7853732d503e45
Luis Calona <calona.luis@gmail.com>
2018-10-13 01:09:27 -0700

    Added menu

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './css/App.css';
+import logo from '../logo.svg';
+import '../css/App.css';
+import MenuHeaderExample from './Menu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <MenuHeaderExample/>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
-import App from './App';
+import 'semantic-ui-css/semantic.min.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
