80dd1908ac1e1a28fb030b731088617edafd11aa
Luis Calona <calona.luis@gmail.com>
2018-10-13 21:55:13 -0700

    Style login button

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,35 +1,23 @@
-import React, { Component } from 'react';
-import { Button, Icon } from 'semantic-ui-react';
+import React, {Component} from 'react';
+import {Button, Icon} from 'semantic-ui-react';
 import {login} from '../server/api';
 
 export class LoginButton extends Component {
-
-    //chesterBad = () => {
-    //    console.log(provider);
-    //    auth.signInWithPopup(provider).then((result) => {
-    //        console.log(result);
-    //    });
-    //}
-
-    // testFunction() {
-    //     const provider = new firebase.auth.GoogleAuthProvider();
-
-    //     console.log('provider');
-    //     console.log(provider);
-
-    //     auth.signInWithPopup(provider).then((result) => {
-    //         console.log(result);
-    //     });
-    // }
-
-    render() {
-        return(
-            <React.Fragment>
-                <Button onClick={login}>
-                <Icon name='google' />
-                Log In with Google
+  render() {
+    return (
+      <React.Fragment>
+        <Button
+          size='small'
+          compact
+          icon
+          labelPosition='left'
+          color='google plus'
+          onClick={login}
+        >
+          <Icon name='google' />
+          Log In with Google
                 </Button>
-            </React.Fragment>
-        );
-    }
+      </React.Fragment>
+    );
+  }
 }
