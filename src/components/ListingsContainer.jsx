c215b7f9e68efd1d8d6cbabadeaf5c0ee6967c77
Luis Calona <calona.luis@gmail.com>
2018-10-14 03:36:52 -0700

    Added image placeholder

--- src/components/ListingsContainer.jsx
+++ src/components/ListingsContainer.jsx
@@ -4,16 +4,19 @@ import {ListingCard} from './ListingCard';
 
 export default class ListingsContainer extends Component {
   render() {
-    const imgSrc = 'https://react.semantic-ui.com/images/wireframe/image.png';
+    const imgPlaceholder = 'https://react.semantic-ui.com/images/wireframe/image.png';
     const descriptionPlaceholder = <Image src='https://react.semantic-ui.com/images/wireframe/short-paragraph.png' />;
+
     return (
       this.props.listings.map((listing) => (
         <ListingCard
           key={listing.title}
           header={listing.title}
           meta={listing.price}
-          imgSrc={"https://maps.googleapis.com/maps/api/staticmap?center="
-            + listing.latitude + "," + listing.longitude + "&zoom=14&size=400x300&key=AIzaSyD-X42BKQuA-ao5n2sNFbaz0vQabOkRX0s"}
+          imgSrc={(listing.latitude && listing.latitude)
+            ? "https://maps.googleapis.com/maps/api/staticmap?center=" + listing.latitude + "," + listing.longitude + "&zoom=14&size=400x300&key=AIzaSyD-X42BKQuA-ao5n2sNFbaz0vQabOkRX0s"
+            : imgPlaceholder
+          }
           description={listing.description ? listing.description : descriptionPlaceholder}
         />
       ))
